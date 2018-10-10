e9f436ebb5f05b614b82552d6ac49a69191c2f4b
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2018-10-09 22:48:02 -0700

    update streaks for 0 value

--- src/helpers/longestStreak.js
+++ src/helpers/longestStreak.js
@@ -50,13 +50,12 @@ const compare = (a, b) => {
 };
 
 const decideFillValue = (topStreaks) => {
-  console.log(topStreaks);
-    if (topStreaks[0]) {
+    if (topStreaks[0] && topStreaks[0].streak > 0) {
       topStreaks[0].fillValue = 80;
       topStreaks[0].color = '#FE5F55';
     }
 
-    if (topStreaks[1]) {
+    if (topStreaks[1] && topStreaks[1].streak > 0) {
       topStreaks[1].color = '#17BEBB';
       if (topStreaks[0].streak === topStreaks[1].streak) {
         topStreaks[1].fillValue = 80;
@@ -65,7 +64,7 @@ const decideFillValue = (topStreaks) => {
       }
     }
 
-    if (topStreaks[2]) {
+    if (topStreaks[2] && topStreaks[2].streak > 0) {
       topStreaks[2].color = '#EAC435';
       if (topStreaks[2].streak === topStreaks[1].streak) {
         topStreaks[2].fillValue = topStreaks[1].fillValue;
